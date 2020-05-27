8e2c1cfc08b8d70e799998eb91c9feb1ce7d47db
mariamfh9 <mfh.haider@gmail.com>
2020-05-27 09:43:00 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show 
+    @name = Student.find_by_id(params[:id]).to_s
+      
+  end 
 end
